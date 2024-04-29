510a3c4038ff906c0a0cd8f0c69b3aa27d0513de
Gary Bright <garybright@rocketmail.com>
2024-04-29 01:09:18 -0400

    finished adding the init for server.js

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+Db.once('once', () => {
+    app.listen(PORT, () => {
+        console.log(`Now listening to http://localhost:${PORT}`);
+    });
+});
