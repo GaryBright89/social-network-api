8c137b2d940542cfb4b1cf67037da3f5d14d26b1
Gary Bright <garybright@rocketmail.com>
2024-04-29 18:26:39 -0400

    added models js and index.js, set up export for models

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const routes = require('./routes');
-
-const PORT = process.env.PORT || 3001;
+const express = require("express");
+const routes = require("./routes");
+const db = require("./config/connection.js");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-Db.once('once', () => {
-    app.listen(PORT, () => {
-        console.log(`Now listening to http://localhost:${PORT}`);
-    });
+app.listen(PORT, () => {
+  console.log(`Now listening to http://localhost:${PORT}`);
 });
