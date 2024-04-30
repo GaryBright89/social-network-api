8a02144bd57ca060e542f2bfb3fae2f4fd3650a4
Gary Bright <garybright@rocketmail.com>
2024-04-29 23:15:14 -0400

    added model for user and thought.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkAPI');
+mongoose.connect('mongodb://127.0.0.1:27017/socialnetworkapi');
 
 module.exports = mongoose.connection;
